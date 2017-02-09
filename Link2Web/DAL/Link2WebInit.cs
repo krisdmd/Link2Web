bddd03450b97aac1e8fbb4ff2efefc80530062bb
krisdmd <krisdmd@gmail.com>
2017-02-09 10:06:00 +0100

    Created Link2Web Solution

--- Link2Web/DAL/Link2WebInit.cs
+++ Link2Web/DAL/Link2WebInit.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Web;
 using Link2Web.Models;
 
 namespace Link2Web.DAL
