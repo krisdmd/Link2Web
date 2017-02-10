c2fbbd00860c3d21bdf21d8fc90dc78db43bad79
krisdmd <krisdmd@gmail.com>
2017-02-10 15:47:27 +0100

    Adding Analytics Models

---
+++ Link2Web/Models/AnalyticsChannels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Link2Web.Models
+{
+    public class AnalyticsChannels
+    {
+        public int AnalyticsChannelsId { get; set; }
+        public string Name { get; set; }
+        [DefaultValue(true)]
+        [ScaffoldColumn(false)]
+        public bool Visible { get; set; }
+
+        public ICollection<AnalyticsAllTraffic> AnalyticsAllTraffic { get; set; }
+    }
+}
---
+++ Link2Web/Models/AnalyticsSocial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Link2Web.Models
+{
+    public class AnalyticsSocial
+    {
+        public int AnalyitcsSocialId { get; set; }
+        public AnalyticsSocialNetwork  AnalyticsSocialNetwork { get; set; }
+        public double GoalValue { get; set; }
+    }
+}
