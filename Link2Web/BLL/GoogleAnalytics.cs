3c81e9315a9b1499d268651090a3c0b9e8175679
etsinformatics <kris.saelen@ets-informatics.be>
2017-03-01 09:29:05 +0100

    Added Analytics Data Class

--- Link2Web/BLL/GoogleAnalytics.cs
+++ Link2Web/BLL/GoogleAnalytics.cs
@@ -1,11 +1,103 @@
-﻿using System;
+﻿using Google.Apis.Analytics.v3;
+using Google.Apis.Analytics.v3.Data;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Link2Web.BLL
 {
     public class GoogleAnalytics
     {
+        public AnalyticsService Service { get; set; }
+
+
+
+        public AnalyticDataPoint GetAnalyticsData(string profileId, string[] dimensions, string[] metrics, DateTime startDate, DateTime endDate)
+        {
+            AnalyticDataPoint data = new AnalyticDataPoint();
+            if (!profileId.Contains("ga:"))
+                profileId = string.Format("ga:{0}", profileId);
+
+            //Make initial call to service.
+            //Then check if a next link exists in the response,
+            //if so parse and call again using start index param.
+            GaData response = null;
+            do
+            {
+                int startIndex = 1;
+                if (response != null && !string.IsNullOrEmpty(response.NextLink))
+                {
+                    Uri uri = new Uri(response.NextLink);
+                    var paramerters = uri.Query.Split('&');
+                    string s = paramerters.First(i => i.Contains("start-index")).Split('=')[1];
+                    startIndex = int.Parse(s);
+                }
+
+                var request = BuildAnalyticRequest(profileId, dimensions, metrics, startDate, endDate, startIndex);
+                response = request.Execute();
+                data.ColumnHeaders = response.ColumnHeaders;
+                data.Rows.AddRange(response.Rows);
+
+            } while (!string.IsNullOrEmpty(response.NextLink));
+
+            return data;
+        }
+
+        private DataResource.GaResource.GetRequest BuildAnalyticRequest(string profileId, string[] dimensions, string[] metrics,
+                                                                            DateTime startDate, DateTime endDate, int startIndex)
+        {
+            DataResource.GaResource.GetRequest request = Service.Data.Ga.Get(profileId, startDate.ToString("yyyy-MM-dd"),
+                                                                                endDate.ToString("yyyy-MM-dd"), string.Join(",", metrics));
+            request.Dimensions = string.Join(",", dimensions);
+            request.StartIndex = startIndex;
+            return request;
+        }
+
+        public IList<Profile> GetAvailableProfiles()
+        {
+            var response = Service.Management.Profiles.List("~all", "~all").Execute();
+            return response.Items;
+        }
+
+        public class AnalyticDataPoint
+        {
+            public AnalyticDataPoint()
+            {
+                Rows = new List<IList<string>>();
+            }
+
+            public IList<GaData.ColumnHeadersData> ColumnHeaders { get; set; }
+            public List<IList<string>> Rows { get; set; }
+        }
+
+        /// <summary>
+        /// Get the GoogleAnalytics visitors from a specific datetime
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDatet"></param>
+        /// <returns>Return a List from Google Analytics with raw data</returns>
+        public AnalyticDataPoint GetVisitorsByDate(DateTime startDate, DateTime endDate)
+        {
+            var data = new AnalyticDataPoint();
+            var analyticsData = new GoogleAnalytics();
+            var dimensions = new []
+            {
+                "ga:date"
+            };
+            var metrics = new[]
+            {
+                "ga:users",
+                "ga:adClicks",
+                "ga:bounceRate"
+            };
+
+
+            if (Service != null)
+            {
+                analyticsData.Service = Service;
+                data = analyticsData.GetAnalyticsData("ga:136022774", dimensions, metrics, startDate, endDate);
+            }
+
+            return data;
+        }
     }
 }
---
+++ Link2Web/Controllers/AnalyticsController.cs
@@ -0,0 +1,103 @@
+﻿using Google.Apis.Analytics.v3;
+using Link2Web.BLL;
+using System;
+using System.Web.Mvc;
+
+namespace Link2Web.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private AnalyticsService _analyticsService { get; set; }
+
+
+        public ActionResult ShowData()
+        {
+            var analyticsData = new GoogleAnalytics();
+            var data = analyticsData.GetVisitorsByDate(DateTime.Now.AddDays(-180), DateTime.Now);
+
+
+            //data.ColumnHeaders.FirstOrDefault().Name
+
+            return View();
+        }
+
+
+        // GET: Analytics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Analytics/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Analytics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Analytics/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Analytics/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Analytics/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Analytics/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Analytics/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
