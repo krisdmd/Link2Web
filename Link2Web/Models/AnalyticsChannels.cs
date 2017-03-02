4dbdaa0aaa7f588a64dc418bae0a2f2e77ff73cc
krisdmd@gmail.com <krisdmd@gmail.com>
2017-03-02 21:26:40 +0100

    fixed missing packages

--- Link2Web/Models/AnalyticsChannels.cs
+++ Link2Web/Models/AnalyticsChannels.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace Link2Web.Models
 {
@@ -15,6 +12,6 @@ namespace Link2Web.Models
         [ScaffoldColumn(false)]
         public bool Visible { get; set; }
 
-        public ICollection<AnalyticsAllTraffic> AnalyticsAllTraffic { get; set; }
+        public ICollection<AnalyticsVisitors> AnalyticsVisitors { get; set; }
     }
 }
