94402c5db8b8ba9fe6f83cfcbc73552e03083a0a
krisdmd <krisdmd@gmail.com>
2017-03-29 16:29:31 +0200

    Added controllers to admin area

---
+++ Link2Web/Areas/Admin/Controllers/CurrenciesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Link2Web.DAL;
+using Link2Web.Models;
+
+namespace Link2Web.Areas.Admin.Controllers
+{
+    public class CurrenciesController : Controller
+    {
+        private Link2WebDbContext db = new Link2WebDbContext();
+
+        // GET: Admin/Currencies
+        public ActionResult Index()
+        {
+            return View(db.Currencies.ToList());
+        }
+
+        // GET: Admin/Currencies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Currency currency = db.Currencies.Find(id);
+            if (currency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(currency);
+        }
+
+        // GET: Admin/Currencies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Currencies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CurrencyId,Code,Name,Symbol")] Currency currency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Currencies.Add(currency);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(currency);
+        }
+
+        // GET: Admin/Currencies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Currency currency = db.Currencies.Find(id);
+            if (currency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(currency);
+        }
+
+        // POST: Admin/Currencies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CurrencyId,Code,Name,Symbol")] Currency currency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(currency).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(currency);
+        }
+
+        // GET: Admin/Currencies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Currency currency = db.Currencies.Find(id);
+            if (currency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(currency);
+        }
+
+        // POST: Admin/Currencies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Currency currency = db.Currencies.Find(id);
+            db.Currencies.Remove(currency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
