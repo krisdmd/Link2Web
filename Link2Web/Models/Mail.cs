41a1d2a194c8b562f07cbb2fd6e8327833bb5333
krisdmd <krisdmd@gmail.com>
2017-05-19 15:59:36 +0200

    Mailer Class

---
+++ Link2Web/Models/Mail.cs
@@ -0,0 +1,16 @@
+﻿namespace Link2Web.Models
+{
+    public class Mail
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string Host { get; set; }
+        public static int Port { get; set; }
+        public static bool SSL { get; set; }
+
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+    }
+}
